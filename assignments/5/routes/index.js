145e3ad8041207dc474c8faa843e8d54c6005022
Nick Wendt <102791771+Mystic2122@users.noreply.github.com>
2025-12-10 18:50:04 -0800

    fixed routes and redirected logged-in users to home page

--- assignments/5/routes/index.js
+++ assignments/5/routes/index.js
@@ -5,11 +5,20 @@
 const express = require('express');
 const router = express.Router();
 
-// Home page route
+// Home page route (index)
 router.get('/', (req, res) => {
+  console.log('[/] req.session.user =', req.session?.user);
+
+  // If user is logged in, send them to /home
+  if (req.session && req.session.user) {
+    return res.redirect('/home');
+  }
+
+  // If not logged in, show the original index page
   res.render('index', { 
     title: 'Home',
-    message: 'Welcome to the Authentication Template'
+    message: 'Welcome to the Authentication Template',
+    isAuthenticated: false
   });
 });
 
@@ -17,7 +26,17 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
   res.render('about', { 
     title: 'About',
-    message: 'Learn about this application'
+    message: 'Learn about this application',
+    isAuthenticated: req.session.user
+  });
+});
+
+// Home page (movie site) route
+router.get('/home', (req, res) => {
+  res.render('home', {
+    title: 'Home Page',
+    message: 'Welcome to the Home Page!',
+    isAuthenticated: req.session.user
   });
 });
 
